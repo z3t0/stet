436c8f76db63cc822f69cf6c779ec775d63e16ad
Rafi Khan <zetoslab@gmail.com>
2017-06-25 00:24:36 -0600

    the request handler is multithreaded, working on #2

--- src/server.cpp
+++ src/server.cpp
@@ -1,5 +1,7 @@
 #include "server.hpp"
 #include <iostream>
+#include <pthread.h>
+#include <unistd.h>
 
 #include <zmq.hpp>
 #include <string>
@@ -10,6 +12,8 @@
 #define sleep(n) Sleep(n)
 #endif
 
+void *request_handler(void *arg);
+
 
 Server::Server(const std::string address) : m_address(address) {
 
@@ -17,21 +21,48 @@ Server::Server(const std::string address) : m_address(address) {
 
 void Server::init() {
   zmq::context_t context (1);
-  zmq::socket_t socket (context, ZMQ_REP);
-  socket.bind(m_address);
+  zmq::socket_t clients(context, ZMQ_ROUTER);
+  clients.bind(m_address);
+  zmq::socket_t workers (context, ZMQ_DEALER);
+  workers.bind("inproc://workers");
 
   std::cout << "Server Initialized" << std::endl;
 
-  while (true) {
-
-    zmq::message_t request;
-
-    // wait for request
-    socket.recv(&request);
-    std::cout << "Received" << std::endl;
-
-    zmq::message_t reply (5);
-    memcpy(reply.data(), "World", 5);
-    socket.send(reply);
+  // Launch workers
+  for (int thread_nbr = 0; thread_nbr != 5; thread_nbr++) {
+    pthread_t worker;
+    pthread_create(&worker, NULL, request_handler, (void *) &context);
   }
+
+  zmq::proxy(clients, workers, NULL);
+  
+}
+
+
+void *request_handler(void *arg) {
+
+  std::cout << "Worker thread initialized" << std::endl;
+
+  zmq::context_t *context = (zmq::context_t *) arg;
+
+  zmq::socket_t socket (*context, ZMQ_REP);
+  socket.connect("inproc://workers");
+
+  while(true) {
+    // Wait for next request
+    zmq::message_t request;
+    socket.recv (&request);
+    std::cout << "Received request: [" << (char*) request.data() << "]" << std::endl;
+
+    // Do somw work
+
+    // Send reply back
+    zmq::message_t reply (6);
+    memcpy((void *) reply.data(), "World", 6);
+    socket.send(reply);
+
+  }
+
+  return (NULL);
+
 }
